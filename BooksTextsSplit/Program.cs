885746510a827b66b374d9ab2c012e698abd0f04
YUGCHR <47811171+YUGCHR@users.noreply.github.com>
2020-07-22 18:55:23 +0100

    Redis client added

--- BooksTextsSplit/Program.cs
+++ BooksTextsSplit/Program.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
 
 namespace BooksTextsSplit
 {
@@ -13,6 +15,47 @@ namespace BooksTextsSplit
     {
         public static void Main(string[] args)
         {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost,abortConnect=false");
+            IDatabase db = redis.GetDatabase();
+            db.StringSet(BitConverter.GetBytes(5), "asdf");            
+
+            // Perform cache operations using the cache object...
+
+            // Simple PING command
+            string cacheCommand = "PING";
+            Console.WriteLine("\nCache command  : " + cacheCommand);
+            Console.WriteLine("Cache response : " + db.Execute(cacheCommand).ToString());
+
+            // Simple get and put of integral data types into the cache
+            cacheCommand = "GET Message";
+            Console.WriteLine("\nCache command  : " + cacheCommand + " or StringGet()");
+            Console.WriteLine("Cache response : " + db.StringGet("Message").ToString());
+
+            cacheCommand = "SET Message \"Hello! The cache is working from a .NET console app!\"";
+            Console.WriteLine("\nCache command  : " + cacheCommand + " or StringSet()");
+            Console.WriteLine("Cache response : " + db.StringSet("Message", "Hello! The cache is working from a .NET console app!").ToString());
+
+            // Demonstrate "SET Message" executed as expected...
+            cacheCommand = "GET Message";
+            Console.WriteLine("\nCache command  : " + cacheCommand + " or StringGet()");
+            Console.WriteLine("Cache response : " + db.StringGet("Message").ToString());
+
+            // Get the client list, useful to see if connection list is growing...
+            // Note that this requires the allowAdmin=true
+            cacheCommand = "CLIENT LIST";
+            Console.WriteLine("\nCache command  : " + cacheCommand);
+            var endpoint = (System.Net.DnsEndPoint)redis.GetEndPoints()[0];
+            var server = redis.GetServer(endpoint.Host, endpoint.Port);
+
+            //var clients = server.ClientList();
+            //Console.WriteLine("Cache response :");
+            //foreach (var client in clients)
+            //{
+            //    Console.WriteLine(client.Raw);
+            //}
+            //
+            //lazyConnection.Value.Dispose();
+
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -21,6 +64,7 @@ namespace BooksTextsSplit
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                });        
     }
+
 }
