1ee95926e097c9331f9dce2dc32c0a8b28878a0c
Yuriy <47811171+YUGCHR@users.noreply.github.com>
2020-11-02 16:51:11 +0000

    AnalyseTextBook is separate from RecordFileToDbInBackground

--- BooksTextsSplit/Services/BackgroungTasksService.cs
+++ BooksTextsSplit/Services/BackgroungTasksService.cs
@@ -37,27 +37,12 @@ namespace BooksTextsSplit.Services
         {
             JsonSerializerOptions options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true, };
             TextSentence bookDescription = JsonSerializer.Deserialize<TextSentence>(jsonBookDescription, options);
+            int desiredTextLanguage = bookDescription.LanguageId;
+
             string fileName = bookFile.FileName;
             StreamReader reader = new StreamReader(bookFile.OpenReadStream());
             string text = reader.ReadToEnd();
 
-            IAllBookData _bookData = new AllBookData();
-            ITextAnalysisLogicExtension analysisLogic = new TextAnalysisLogicExtension(_bookData);
-            ISentencesDividingAnalysis sentenceAnalyser = new SentencesDividingAnalysis(_bookData, analysisLogic);
-            //IAnalysisLogicParagraph paragraphAnalysis = new noneAnalysisLogicParagraph(bookData, msgService, analysisLogic);
-            IChapterDividingAnalysis chapterAnalyser = new ChapterDividingAnalysis(_bookData, analysisLogic);
-            IAllBookAnalysis bookAnalysis = new AllBookAnalysis(_bookData, analysisLogic, chapterAnalyser, sentenceAnalyser);
-
-            int desiredTextLanguage = bookDescription.LanguageId;
-            //создание нужной инструкции ToDo
-            _bookData.SetFileToDo((int)WhatNeedDoWithFiles.AnalyseText, desiredTextLanguage);
-            //bookData.SetFilePath(_filePath, desiredTextLanguage);
-
-            //List<TextSentence> requestedSelectResult = await cache.Cache.FetchObjectAsync<List<TextSentence>>(booksPairTextsKey, () => FetchBooksTextsFromDb(where1, where1Value));
-            //await cache.Cache.SetObjectAsync(createdKeyNameFromRequest, requestedSelectResult, TimeSpan.FromDays(1));
-
-            string fileContent = text;
-            _bookData.SetFileContent(fileContent, desiredTextLanguage);
             // Enqueue a background work item
             _taskQueue.QueueBackgroundWorkItem(async token =>
             {
@@ -67,18 +52,18 @@ namespace BooksTextsSplit.Services
                     "Queued Background Task RecordFileToDb {Guid} is starting", guid);
 
                     // TODO it's necessary to pass bookDescription to AnalyseTextBook and use bookDescription.properties when initielize textSentences[]
-                    TextSentence[] textSentences = bookAnalysis.AnalyseTextBook();
+                    TextSentence[] textSentences = AnalyseTextBook(text, desiredTextLanguage);
                     int textSentencesLength = textSentences.Length;
-                    string json = JsonSerializer.Serialize(textSentences);
+                    //string json = JsonSerializer.Serialize(textSentences);
                     int currentUploadingVersion = bookDescription.UploadVersion + 1;
 
                     TaskUploadPercents uploadPercents = new TaskUploadPercents
                     {
+                        DoneInPercents = 0,
+                        CurrentUploadingRecord = 0,
+                        RecordrsTotalCount = textSentencesLength,
                         CurrentTaskGuid = guid,
                         CurrentUploadingBookId = bookDescription.BookId, // may be put whole TextSentence?
-                        RecordrsTotalCount = textSentencesLength,
-                        CurrentUploadingRecord = 0,
-                        DoneInPercents = 0,
                     };
 
                     try
@@ -87,13 +72,16 @@ namespace BooksTextsSplit.Services
                             "Queued Background Task RecordFileToDb {Guid} is running", guid);
 
                         var tackKeyForRedis = guid;
-                        double doneInPercents;
+                        //double doneInPercents;
                         int percentPrevious = 0;
                         int percentCurrent;
                         int percentDecrement = 0;
 
                         for (int tsi = 0; tsi < textSentencesLength; tsi++)
                         {
+                            // Check the time of one cycle, calculate the whole task run time, 
+                            // if it more 10 sec, percents will be shown - 1 state per second
+
                             textSentences[tsi].Id = Guid.NewGuid().ToString();
                             textSentences[tsi].BookId = bookDescription.BookId;
                             textSentences[tsi].AuthorNameId = bookDescription.AuthorNameId;
@@ -102,15 +90,13 @@ namespace BooksTextsSplit.Services
                             textSentences[tsi].BookName = bookDescription.BookName;
 
                             textSentences[tsi].UploadVersion = currentUploadingVersion;
-
-                            // TODO add the key with percents to Redis
-                            doneInPercents = tsi * 10000.0 / textSentencesLength;
-                            percentCurrent = Convert.ToInt32(doneInPercents / 100);
+ 
+                            percentCurrent = Convert.ToInt32(tsi * 10000.0 / textSentencesLength / 100.0);
                             if (percentCurrent >= percentPrevious + percentDecrement)
                             {
-                                uploadPercents.CurrentUploadingRecord = tsi;
+                                uploadPercents.CurrentUploadingRecord = tsi; // for debug only
                                 uploadPercents.DoneInPercents = percentCurrent;
-                                _logger.LogInformation("Task RecordFileToDb {Guid} recorded " + percentCurrent.ToString() + " % to DB", guid);
+                                // _logger.LogInformation("Task RecordFileToDb {Guid} recorded " + percentCurrent.ToString() + " % to DB", guid);
                                 await cache.Cache.SetObjectAsync(tackKeyForRedis, uploadPercents, TimeSpan.FromDays(1));
                                 percentPrevious = percentCurrent;
                             }
@@ -134,6 +120,36 @@ namespace BooksTextsSplit.Services
                 }
             });
             //return "Task " + guid + " was Queued Background";
+        }
+
+        public void DoneInPercents()
+        {
+            
+        }
+
+        public TextSentence[] AnalyseTextBook(string text, int desiredTextLanguage)
+        {
+            
+
+            IAllBookData _bookData = new AllBookData();
+            ITextAnalysisLogicExtension analysisLogic = new TextAnalysisLogicExtension(_bookData);
+            ISentencesDividingAnalysis sentenceAnalyser = new SentencesDividingAnalysis(_bookData, analysisLogic);
+            //IAnalysisLogicParagraph paragraphAnalysis = new noneAnalysisLogicParagraph(bookData, msgService, analysisLogic);
+            IChapterDividingAnalysis chapterAnalyser = new ChapterDividingAnalysis(_bookData, analysisLogic);
+            IAllBookAnalysis bookAnalysis = new AllBookAnalysis(_bookData, analysisLogic, chapterAnalyser, sentenceAnalyser);
+
+            //int desiredTextLanguage = bookDescription.LanguageId;
+            //создание нужной инструкции ToDo
+            _bookData.SetFileToDo((int)WhatNeedDoWithFiles.AnalyseText, desiredTextLanguage);
+            //bookData.SetFilePath(_filePath, desiredTextLanguage);
+
+            //List<TextSentence> requestedSelectResult = await cache.Cache.FetchObjectAsync<List<TextSentence>>(booksPairTextsKey, () => FetchBooksTextsFromDb(where1, where1Value));
+            //await cache.Cache.SetObjectAsync(createdKeyNameFromRequest, requestedSelectResult, TimeSpan.FromDays(1));
+
+            string fileContent = text;
+            _bookData.SetFileContent(fileContent, desiredTextLanguage);
+
+            return bookAnalysis.AnalyseTextBook();
         }
 
         public void WorkerSample(string guid)
